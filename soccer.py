68d5a2bcac3c02bddeb3d64fdf83b6a1f0bbc321
countrygirlaz06 <countrygirlaz06@gmail.com>
2026-03-09 23:39:04 -0600

    Create soccer.py

---
+++ soccer.py
@@ -0,0 +1,10 @@
+#Ashlyn Crop
+
+#Git Hub Soccer Project
+
+# Function One
+def welcome() :
+    home_name = input("Please enter in your team name: ")
+    print(f"Welcome {home_name} to your soccer season Predictions! \n")
+    return home_name
+print("You will enter in the number of games you will play and your opponants name")
